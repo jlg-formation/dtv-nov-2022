7c6669fa61d896c50800d00b2a644de8355368ee
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-30 17:24:27 +0100

    dev: explore segment ok

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,4 +1,8 @@
 import { Router } from "express";
+import got from "got";
+
+const stravaUrl = "https://www.strava.com/api/v3";
+const token = process.env.LL_STRAVA_TOKEN || "not_defined";
 
 const app = Router();
 
@@ -6,6 +10,28 @@ app.get("/date", (req, res) => {
   res.json({
     date: new Date(),
   });
+});
+
+app.get("/strava/segments/explore", (req, res) => {
+  (async () => {
+    try {
+      const response = await got
+        .get(stravaUrl + "/segments/explore", {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+          searchParams: {
+            bounds: "0,0,10,10",
+          },
+        })
+        .json();
+      console.log("response: ", response);
+      res.json(response);
+    } catch (err) {
+      console.log("err: ", err);
+      res.status(500).end();
+    }
+  })();
 });
 
 export const api = app;
