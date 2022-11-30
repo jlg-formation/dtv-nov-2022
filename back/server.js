3f07ddfab6cd71d4e99e320f61e35f65a692b55d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-30 14:33:16 +0100

    dev: added server node

--- back/server.js
+++ back/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const serveIndex = require("serve-index");
 
 const app = express();
 const port = 3000;
+const wwwDir = ".";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log("req: ", req);
+  res.setHeader("X-Truc", "bidule");
+  next();
 });
+
+app.use(express.static(wwwDir));
+app.use(serveIndex(wwwDir, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
