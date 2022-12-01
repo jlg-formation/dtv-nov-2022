dfd9fc9df55d0cf9d0a2ccee2fc322e56e69e121
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-12-01 11:08:52 +0100

    dev: css layout

--- front/src/components/LegendMap.vue
+++ front/src/components/LegendMap.vue
@@ -1,3 +1,24 @@
 <template>
-  <p>Coucou composant !</p>
+  <div class="legend-map">
+    <div class="list">Liste</div>
+    <div class="map">Carte</div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.legend-map {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+
+  .list {
+    background-color: red;
+    width: 20em;
+  }
+
+  .map {
+    background-color: blue;
+    flex: 1;
+  }
+}
+</style>
